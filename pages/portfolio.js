5a9f95c8b4c0efadac432185a28a4d85f9a119c1
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-10-27 00:27:34 +0100

    added new content

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,4 +1,5 @@
 import { useEffect, Fragment } from "react";
+import Link from "next/link";
 import Header from "../components/header/Header";
 import Skill from "../components/skill/Skill";
 import './portfolio.css'
@@ -16,6 +17,18 @@ const Portfolio = () => {
             </div>
 
             <Skill />
+
+            <div className="portfolio-myworks">
+                <Link href="https://docs.google.com/spreadsheets/d/1cjc1YIpO4q-TicoFtAHAPD72uBNxdpUa/edit#gid=1270802026" className="portfolio-card">
+                    <div className="portfolio-card-image">
+                        <img src="/cust_segment.png" alt="banner Image" />
+                    </div>
+                    <div className="portfolio-card-content">
+                        <p className="portfolio-card-title">Excel: Customer Segmentation Analysis</p>
+                        <p className="portfolio-card-subtitle">A survey was conducted among students enrolled in various nanodegree programs on an e-learning platform. By analyzing the collected data, valuable insights were gathered, empowering data-driven marketing decisions that will ultimately boost sales for the e-learning platform.</p>
+                    </div>
+                </Link>
+            </div>
         </Fragment>
     )
 }
