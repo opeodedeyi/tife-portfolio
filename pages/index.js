dbcfab13ac21077addf96214b1bf1134d7d3c10d
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2025-09-29 12:48:52 +0100

    added proper cards for community event page

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,14 @@ const Home = () => {
             </div>
 
             <div className="home-container-socials">
-                <p className="home-container-content">A data-driven professional with a master&apos;s degree in Business Analytics. My expertise lies in utilizing data to uncover valuable insights and drive impactful business decisions.</p>
+                <p className="home-container-content">I am a data-driven professional with a Master&apos;s in Business Analytics and a postgraduate degree in Artificial Intelligence and Machine Learning. I&apos;m passionate about using data to solve business problems, whether by developing analytical solutions or building and deploying models.</p>
+
+                <p className="home-container-content">Known for my strong work ethic, adaptability, data storytelling, critical thinking, and problem-solving skills, I continuously seek opportunities to learn and grow.</p>
+
+                <p className="home-container-content">When I&apos;m not reading an inspirational book, you&apos;ll likely find me working on data science and analytics projects in my spare time.</p>
+
+                <p className="home-container-content">If you&apos;re looking for someone who can turn data into impact and deliver results, I&apos;d be glad to connect. You can also explore some of my projects in my portfolio below.</p>
+
                 <p className="home-normal-text">Do you want to discuss or work with me?</p>
                 <Link href='/contact' className='button-normal'>Let&apos;s Talk</Link>
             </div>
